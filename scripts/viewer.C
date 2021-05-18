1fe65f1ea53ced101e8664efed21b3307d9c7d47
Alexander Kiselev <ayk@bnl.gov>
2021-05-18 21:58:06 +0200

    Minor edits

--- scripts/viewer.C
+++ scripts/viewer.C
@@ -8,35 +8,38 @@ void viewer(const char *fname, double minR = 0.0, double maxR = 400.0,
 
   auto bmf = new BeastMagneticField(fname);
 
-  bool plot[3] = {plotBz, plotBr, plotBm};
-  unsigned divY = plotBz + plotBr + plotBm;
-  auto cv = new TCanvas("cv", "", 0, 0, cvX, cvY); cv->Divide(1,divY);
+  if (bmf->ValidMapImported()) {
+    bool plot[3] = {plotBz, plotBr, plotBm};
+    unsigned divY = plotBz + plotBr + plotBm;
+    auto cv = new TCanvas("cv", "", 0, 0, cvX, cvY); cv->Divide(1,divY);
 
-  unsigned dimR = 200, dimZ = 600;
-  double cellR = (maxR - minR)/dimR, cellZ = (maxZ - minZ)/dimZ;
-
-  auto hbr = new TH2D("hbr", "R-component",     dimZ, minZ, maxZ, dimR, minR, maxR);
-  auto hbz = new TH2D("hbz", "Z-component",     dimZ, minZ, maxZ, dimR, minR, maxR);
-  auto hbm = new TH2D("hbm", "SQRT(BR^2+BZ^2)", dimZ, minZ, maxZ, dimR, minR, maxR);
-  TH2D *hh[3] = {hbz, hbr, hbm};
-
-  for(unsigned ir=0; ir<dimR; ir++)
-    for(unsigned iz=0; iz<dimZ; iz++) {
-      double br, bz;
-      bmf->GetFieldValue(minR + cellR*(ir + 0.5), minZ + cellZ*(iz + 0.5), br, bz);
-
-      hbz->SetBinContent(iz+1, ir+1, bz);
-      hbr->SetBinContent(iz+1, ir+1, br);
-      hbm->SetBinContent(iz+1, ir+1, sqrt(bz*bz+br*br));
-    } //for ir..iz
+    unsigned dimR = 200, dimZ = 600;
+    double cellR = (maxR - minR)/dimR, cellZ = (maxZ - minZ)/dimZ;
+    
+    auto hbr = new TH2D("hbr", "R-component",     dimZ, minZ, maxZ, dimR, minR, maxR);
+    auto hbz = new TH2D("hbz", "Z-component",     dimZ, minZ, maxZ, dimR, minR, maxR);
+    auto hbm = new TH2D("hbm", "SQRT(BR^2+BZ^2)", dimZ, minZ, maxZ, dimR, minR, maxR);
+    TH2D *hh[3] = {hbz, hbr, hbm};
+    
+    for(unsigned ir=0; ir<dimR; ir++)
+      for(unsigned iz=0; iz<dimZ; iz++) {
+	double br, bz;
+	bmf->GetFieldValue(minR + cellR*(ir + 0.5), minZ + cellZ*(iz + 0.5), br, bz);
 	
-  {
-    unsigned iYcurrent = 1;
-
-    for(unsigned iy=0; iy<3; iy++) 
-      if (plot[iy]) {
-	cv->cd(iYcurrent++);
-	hh[iy]->Draw("COLZ");
-      } 
-  }
+	hbz->SetBinContent(iz+1, ir+1, bz);
+	hbr->SetBinContent(iz+1, ir+1, br);
+	hbm->SetBinContent(iz+1, ir+1, sqrt(bz*bz+br*br));
+      } //for ir..iz
+    
+    {
+      unsigned iYcurrent = 1;
+      
+      for(unsigned iy=0; iy<3; iy++) 
+	if (plot[iy]) {
+	  cv->cd(iYcurrent++);
+	  hh[iy]->Draw("COLZ");
+	} 
+    }
+  } else
+    printf("\n  Was not able to import the field map!\n");
 } // viewer()
